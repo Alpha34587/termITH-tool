311aa6ad6f825094a3b97272c4d65af60112f643
Simon Meoni <simonmeoni@aol.com>
2016-06-15 16:51:36 +0200

    clean repo

--- scripts/sh/functions/declarer-variable.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-function declarer-variable {
-    touch "$disciplines/$1/phase0/fichier_correspondance.csv"
-    declare -Ag c_array
-    echo "Traitement ..."
-}
--- scripts/sh/functions/normalisation-tei.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-function normalisation-tei {
-    $saxon "-s:$f" "-xsl:$xsl/$2" "-o:$disciplines/$1/phase1/`basename $f`" 2> /dev/null
-}
