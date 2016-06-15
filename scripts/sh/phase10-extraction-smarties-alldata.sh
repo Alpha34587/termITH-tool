707b64aab0618f715d81ba22a0928c3d96ae7fc2
Simon Meoni <simonmeoni@aol.com>
2016-06-15 09:56:05 +0200

    clean project and remove tree-tagger

--- scripts/sh/phase10-extraction-smarties-alldata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source ../path
-phase="phase10-extraction-smarties-discipline.sh"
-
-./$phase psychologie &
-./$phase archeologie &
-./$phase linguistique &
-wait
--- scripts/sh/phase8-creer-compagnon-termino-alldata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ../path
-phase="phase8-creer-compagnon-termino-discipline.sh"
-./$phase archeologie
-./$phase chimie
-./$phase psychologie
-./$phase linguistique 
-./$phase sciences_information_communication
-./$phase scientext
