7ff89ff516cffb3cf7ea1de9c8ad87fff138399e
Simon Meoni <simonmeoni@aol.com>
2016-06-16 16:18:39 +0200

    update multi word application

--- termith.sh
+++ termith.sh
@@ -3,17 +3,17 @@ source path
 
 if [ ! -z "$1" ]
 then
-  create_folder $1 termith_result
+  # create_folder $1 termith_result
 
-  ./scripts/sh/0_files_validation.sh $1
-  ./scripts/sh/1_extract_text.sh $1
-  ./scripts/sh/2_termsuite_analyze.sh $1 $2
-  ./scripts/sh/3_generate_tokenize_tei.sh $1
-  ./scripts/sh/4_morphosyntax_enrichment.sh $1
-  ./scripts/sh/5_term_enrichment.sh $1
+  # ./scripts/sh/0_files_validation.sh $1
+  # ./scripts/sh/1_extract_text.sh $1
+  # ./scripts/sh/2_termsuite_analyze.sh $1 $2
+  # ./scripts/sh/3_generate_tokenize_tei.sh $1
+  # ./scripts/sh/4_morphosyntax_enrichment.sh $1
+  # ./scripts/sh/5_term_enrichment.sh $1
   ./scripts/sh/6_syntita_projection.sh $1
-  ./scripts/sh/7_transdisplinaries_projection.sh $1
-  ./scripts/sh/8_clean_result_folder.sh $1
+  # ./scripts/sh/7_transdisplinaries_projection.sh $1
+  # ./scripts/sh/8_clean_result_folder.sh $1
 
 else
 
